ca7752b4b5031ac8ff3d99e4281c1168012d4cd5
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2016-07-02 17:57:20 -0500

    Updated vga hander:
      - better clear function, sets background and forground color
      - cursor enable function
      - cursor positioning functions

--- src/vga.c
+++ src/vga.c
@@ -2,6 +2,8 @@
 #include "types.h"
 #include "vga.h"
 
+#define FORMAT_COLORS(fg, bg) (uchar)(fg | (bg << 4))
+
 /***
  * contents of current vga display
  */
@@ -17,6 +19,7 @@ static struct vga_state VGA_STATE;
  */
 void vga_init() {
     VGA_STATE.fg = GREEN, VGA_STATE.bg = BLACK;
+    vga_set_cursor_enable(false);
     vga_clear();
 }
 
@@ -24,8 +27,13 @@ void vga_init() {
  * clear vga screen
  */
 void vga_clear() {
-    memset(VGA_BUF, NULL, VGA_WIDTH * VGA_HEIGHT * 2);
-    VGA_STATE.cursor_x = VGA_STATE.cursor_y = 0;
+    vga_d_char_t *pos, *end;
+    end = VGA_BUF + VGA_HEIGHT * VGA_WIDTH * sizeof(vga_d_char_t);
+    for (pos = VGA_BUF; pos < end; pos++) {
+        (*pos).c = NULL;
+        (*pos).color = FORMAT_COLORS(VGA_STATE.fg, VGA_STATE.bg);
+    }
+    vga_update_cursor(0, 0);
 }
 
 /***
@@ -40,9 +48,42 @@ void vga_newline() {
         for (i = 0; i < VGA_HEIGHT; i++) {
             memcpy((void *)(VGA_BUF + i * VGA_WIDTH),
                    (void *)(VGA_BUF + (i + 1) * VGA_WIDTH),
-                   (size_t)VGA_WIDTH * 2);
+                   (size_t)VGA_WIDTH * sizeof(vga_d_char_t));
         }
     }
+    vga_sync_cursor();
+}
+
+/***
+ * update the vga cursor position
+ */
+void vga_update_cursor(uint16_t x, uint16_t y) {
+    VGA_STATE.cursor_x = x;
+    VGA_STATE.cursor_y = y;
+    vga_sync_cursor();
+}
+
+/***
+ * update the vga cursor in vga registers
+ */
+void vga_sync_cursor() {
+    uint16_t cursor_pos = VGA_STATE.cursor_x + VGA_STATE.cursor_y * VGA_WIDTH;
+    vga_crt_reg_set(CRT_CURSOR_LOC_HIGH,
+            (uchar)((cursor_pos & (0xFF << 8)) >> 8));
+    vga_crt_reg_set(CRT_CURSOR_LOC_LOW, (uchar)(cursor_pos & 0xFF));
+}
+
+/***
+ * either enable or disable vga cursor
+ */
+void vga_set_cursor_enable(uchar enabled) {
+    uchar start_reg_val = vga_crt_reg_get(CRT_CURSOR_START);
+    // enable/disable is bit 5
+    if (enabled)
+        start_reg_val = start_reg_val | 0x20;
+    else
+        start_reg_val = start_reg_val & 0xDF;
+    vga_crt_reg_set(CRT_CURSOR_START, start_reg_val);
 }
 
 /***
@@ -55,9 +96,33 @@ void vga_puts(char *str) {
         // write character and set color
         pos = VGA_BUF + VGA_STATE.cursor_x + (VGA_STATE.cursor_y * VGA_WIDTH);
         (*pos).c = cur;
-        (*pos).color = VGA_STATE.fg | (VGA_STATE.bg << 4);
+        (*pos).color = FORMAT_COLORS(VGA_STATE.fg, VGA_STATE.bg);
         // update cursor position
         if (++VGA_STATE.cursor_x >= VGA_WIDTH)
             vga_newline();
     }
+    vga_sync_cursor();
+}
+
+/***
+ * write data to crt control register at index
+ */
+void vga_crt_reg_set(enum vga_crt_reg_index index, uchar data) {
+    // save previous index value and restore later
+    int16_t prev = inb(VGA_CRT_REG_ADR_PORT);
+    outb(VGA_CRT_REG_ADR_PORT, (int16_t)index);
+    outb(VGA_CRT_REG_DATA_PORT, data);
+    outb(VGA_CRT_REG_ADR_PORT, prev);
+}
+
+/***
+ * read data from crt control register at index
+ */
+uchar vga_crt_reg_get(enum vga_crt_reg_index index) {
+    uchar res;
+    int16_t prev = inb(VGA_CRT_REG_ADR_PORT);
+    outb(VGA_CRT_REG_ADR_PORT, (int16_t)index);
+    res = inb(VGA_CRT_REG_DATA_PORT);
+    outb(VGA_CRT_REG_ADR_PORT, prev);
+    return res;
 }
--- src/vga.h
+++ src/vga.h
@@ -14,6 +14,40 @@
 #endif
 
 /***
+ * VGA ports and crt controller register indices
+ */
+#define VGA_CRT_REG_ADR_PORT 0x3D4
+#define VGA_CRT_REG_DATA_PORT 0x3D5
+
+enum vga_crt_reg_index {
+    CRT_HORIZ_TOTAL         = 0x00,
+    CRT_END_HORIZ_DISP      = 0x01,
+    CRT_START_HORIZ_BLANK   = 0x02,
+    CRT_END_HORIZ_BLANK     = 0x03,
+    CRT_START_HORIZ_RETRACE = 0x04,
+    CRT_END_HORIZ_RETRACE   = 0x05,
+    CRT_VERT_TOTAL          = 0x06,
+    CRT_OVERFLOW            = 0x07,
+    CRT_PRESET_ROW_SCAN     = 0x08,
+    CRT_MAX_SCAN_LINE       = 0x09,
+    CRT_CURSOR_START        = 0x0A,
+    CRT_CURSOR_END          = 0x0B,
+    CRT_START_ADR_HIGH      = 0x0C,
+    CRT_START_ADR_LOW       = 0x0D,
+    CRT_CURSOR_LOC_HIGH     = 0x0E,
+    CRT_CURSOR_LOC_LOW      = 0x0F,
+    CRT_START_VERT_RETRACE  = 0x10,
+    CRT_END_VERT_RETRACE    = 0x11,
+    CRT_END_VERT_DISP       = 0x12,
+    CRT_OFFSET              = 0x13,
+    CRT_UNDERLINE_LOC       = 0x14,
+    CRT_START_VERT_BLANK    = 0x15,
+    CRT_END_VERT_BLANK      = 0x16,
+    CRT_MODE_CONTROL        = 0x17,
+    CRT_LINE_COMPARE        = 0x18,
+};
+
+/***
  * 16 color mode only
  */
 typedef enum vga_color {
@@ -35,14 +69,12 @@ typedef enum vga_color {
     WHITE   = 0xF,
 } vga_color_t;
 
-typedef uchar vga_full_color_t;
-
 /***
  * vga display character, char val and color
  */
 typedef struct vga_d_char {
     char c;
-    vga_full_color_t color;
+    uchar color;
 } vga_d_char_t;
 
 /***
@@ -69,8 +101,33 @@ void vga_clear();
 void vga_newline();
 
 /***
+ * update the vga cursor in vga registers
+ */
+void vga_sync_cursor();
+
+/***
+ * update the vga cursor position
+ */
+void vga_update_cursor(uint16_t x, uint16_t y);
+
+/***
+ * either enable or disable vga cursor
+ */
+void vga_set_cursor_enable(uchar enabled);
+
+/***
  * write text
  */
 void vga_puts(char *str);
+
+/***
+ * write data to crt control register at index
+ */
+void vga_crt_reg_set(enum vga_crt_reg_index index, uchar data);
+
+/***
+ * read data from crt control register at index
+ */
+uchar vga_crt_reg_get(enum vga_crt_reg_index index);
 
 #endif
