4733179e21cbdd5c142048798aee36c9011199dc
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2016-07-02 17:56:55 -0500

    Added a few more typedefs

--- src/types.h
+++ src/types.h
@@ -2,10 +2,22 @@
 #define _TYPES_
 
 #define NULL 0
+#define true 1
+#define false 0
 
-typedef unsigned char uchar;
-typedef unsigned int uint;
+typedef unsigned char   uchar;
+typedef unsigned int    uint;
 
-typedef unsigned int size_t;
+typedef unsigned int    size_t;
+
+typedef char    int8_t;
+typedef short   int16_t;
+typedef int     int32_t;
+typedef long    int64_t;
+
+typedef unsigned char   uint8_t;
+typedef unsigned short  uint16_t;
+typedef unsigned int    uint32_t;
+typedef unsigned long   uint64_t;
 
 # endif
