226f9b96b3df5843ffe6b72f3fcfae2f14eb1a56
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2016-07-02 17:57:03 -0500

    Added inb and outb

--- src/klib.c
+++ src/klib.c
@@ -1,14 +1,38 @@
 #include "klib.h"
 #include "types.h"
 
+/***
+ * Set memory contents at dst with count bytes of value c
+ * Returns write destination
+ */
 void *memset(void *dst, uchar val, size_t count) {
     uint i;
     for (i = 0; i < count; i++) ((uchar *)dst)[i] = val;
     return dst;
 }
 
+/***
+ * Copy count bytes of memory from src to dst
+ * Returns write destination
+ */
 void *memcpy(void *dst, const void *src, size_t count) {
     uint i;
     for (i = 0; i < count; i++) ((uchar *)dst)[i] = ((uchar *)src)[i];
     return dst;
+}
+
+/***
+ * Write byte to port
+ */
+inline void outb(uint16_t port, uchar val){
+    asm volatile("outb %0,%1"::"a"(val), "Nd"(port));
+}
+
+/***
+ * Read byte from port
+ */
+inline uchar inb(uint16_t port) {
+    uchar res;
+    asm volatile("inb %1,%0":"=a"(res):"Nd"(port));
+    return res;
 }
--- src/klib.h
+++ src/klib.h
@@ -3,16 +3,26 @@
 #ifndef _KLIB_
 #define _KLIB_
 
-/**
+/***
  * Set memory contents at dst with count bytes of value c
  * Returns write destination
  */
 void *memset(void *dst, uchar val, size_t count);
 
-/**
+/***
  * Copy count bytes of memory from src to dst
  * Returns write destination
  */
 void *memcpy(void *dst, const void *src, size_t count);
+
+/***
+ * Write byte to port
+ */
+void outb(uint16_t port, uchar val);
+
+/***
+ * Read byte from port
+ */
+uchar inb(uint16_t port);
 
 #endif
