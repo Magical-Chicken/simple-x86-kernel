e9d3e86d70b11800541e55756619684dbfc1ca97
Wesley Wiedenmeier <wesley.wiedenmeier@gmail.com>
2016-07-03 03:00:20 -0500

    Change name of entry function

--- load.asm
+++ load.asm
@@ -6,12 +6,12 @@ section .text
     dd    - (0x1BADB002 + 0x00)
 
 global start
-extern main
+extern entry
 
 start:
     cli
     mov     esp, kstack
-    call    main
+    call    entry
     hlt
 
 section .bss
